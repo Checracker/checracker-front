03e3529616c3ed7df4522f055bb9b5179a2918ae
YeaJeong <bananana0118@gmail.com>
2023-08-05 22:30:11 +0900

    CHEC-13 RTKQuery 세팅

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { store } from "@/provider/store";
+import { Provider } from "react-redux";
+
 type Props = {
   children: React.ReactNode;
 };
 export default function Providers({ children }: Props) {
-  return <>{children}</>;
+  return <Provider store={store}>{children}</Provider>;
 }
