d378e1672931ad4f185ddde72367dc98b2e088d3
YeaJeong <bananana0118@gmail.com>
2023-09-10 01:54:39 +0900

    [CHEC-37] Design : 페이지 스타일링

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -3,7 +3,14 @@
 import FroalaEditor from "@/components/ui/FroalaEditor/FroalaEditor";
 import { theme } from "@/styles/emotionTheme";
 import styled from "@emotion/styled";
-import { Button, Modal } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Modal,
+  Radio,
+} from "@mui/material";
 import { useState } from "react";
 
 type BoardNumber = {
@@ -15,26 +22,90 @@ export default function DetailBoard() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   let tempBoardNumber = 3;
+  const label = {
+    inputProps: { "aria-label": `${tempBoardNumber}사분면목표 A` },
+  };
+
   return (
     <div>
       <Button onClick={handleOpen}>클릭</Button>
       <Modal open={open} onClose={handleClose}>
         <ModalEditor>
-          <Header boardNumber={tempBoardNumber}>
-            <div>{tempBoardNumber}</div>
-            중요x 긴급os
-          </Header>
-          <Hr boardNumber={tempBoardNumber}></Hr>
-          <div>
-            <Title>최종 목표에 대한 내용</Title>
-            <div>목표에 대한 내용을 메모해보세요 // floaraEditor</div>
-          </div>
-          <div>
-            todolist
-            <div>할일추가하기</div>
-            <div>목표 list</div>
-          </div>
-          <FroalaEditor />
+          <TopContainer>
+            <Header boardNumber={tempBoardNumber}>
+              <div>{tempBoardNumber}</div>
+              중요x 긴급os
+            </Header>
+            <Hr boardNumber={tempBoardNumber}></Hr>
+            <div>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Radio style={{ width: 48, height: 48, marginRight:"8px" }} />}
+                  label={
+                    <LabelBox>
+                      <Title>최종 목표에 대한 내용</Title>
+                    </LabelBox>
+                  }
+                />{" "}
+                <div>목표에 대한 내용을 메모해보세요 // floaraEditor</div>
+              </FormGroup>
+            </div>
+            <FroalaEditor />
+          </TopContainer>
+
+          <TodoList>
+            <FormGroup>
+              todolist
+              <AddTodoBtn onClick={() => console.log("할일 추가하기 클릭!")}>
+                <p>+</p> 할 일 추가하기
+              </AddTodoBtn>
+              <TodoItem>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label={
+                    <LabelBox>
+                      <Label>
+                        밤 맛이 나는 바밤바밤 맛이 나는 바밤바밤 맛이 나는
+                        바밤바밤 맛이 나는 바밤바
+                      </Label>
+                    </LabelBox>
+                  }
+                />
+                <IssueBtn>이슈사항</IssueBtn>
+              </TodoItem>
+              <TodoItem>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label={
+                    <LabelBox>
+                      <Label>
+                        밤 맛이 나는 바밤바밤 맛이 나는 바밤바밤 맛이 나는
+                        바밤바밤 맛이 나는 바밤바
+                      </Label>
+                    </LabelBox>
+                  }
+                />
+                <IssueBtn>이슈사항</IssueBtn>
+              </TodoItem>
+              <TodoItem>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label={
+                    <LabelBox>
+                      <Label>
+                        밤 맛이 나는 바밤바밤 맛이 나는 바밤바밤 맛이 나는
+                        바밤바밤 맛이 나는 바밤바
+                      </Label>
+                    </LabelBox>
+                  }
+                />{" "}
+                <IssueBtn>이슈사항</IssueBtn>
+              </TodoItem>
+            </FormGroup>
+          </TodoList>
+          <BottomContainer>
+            <SaveBtn>저장하기</SaveBtn>
+          </BottomContainer>
         </ModalEditor>
       </Modal>
     </div>
@@ -47,17 +118,20 @@ const ModalEditor = styled.div`
   top: 50%;
   left: 50%;
   width: 80%;
-  height: 70%;
   background-color: #ffffff;
   border-radius: 8px;
   box-shadow: 0px 2px 6px 2px #00000026;
   box-shadow: 0px 1px 2px 0px #0000004d;
 `;
-
+const TodoList = styled.div`
+  width: 100%;
+  padding: 16px;
+  background-color: ${({ theme }) => theme.grayScale.bgColor};
+`;
 const Header = styled.div<BoardNumber>`
   display: flex;
   flex-direction: row;
-  color: ${({ boardNumber, theme }) => theme.board[boardNumber - 1].title};
+  color: ${({ boardNumber, theme }) => theme.board[boardNumber - 1].primary};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: ${({ theme }) => theme.fontSize.title}px;
   line-height: 36px;
@@ -65,12 +139,81 @@ const Header = styled.div<BoardNumber>`
 const Title = styled.div`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: ${({ theme }) => theme.fontSize.title}px;
-  color: ${({ theme }) => theme.colors.danger};
+  color: ${({ theme }) => theme.colors.black};
   line-height: 36px;
+  margin: 19px 0;
 `;
 
 const Hr = styled.hr<BoardNumber>`
   border-color: ${({ boardNumber, theme }) =>
     theme.board[boardNumber - 1].line};
   border-width: 1px;
+`;
+
+const AddTodoBtn = styled.div`
+  display: flex;
+  background-color: #a1a1a1;
+  border-radius: 12px;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.white};
+  padding: 0px 16px;
+  height: 50px;
+  gap: 8px;
+`;
+
+const TodoItem = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 0 16px;
+  color: ${({ theme }) => theme.colors.black};
+`;
+const Label = styled.div`
+  width: 500px;
+  overflow: hidden;
+  font-weight: 500;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+const IssueBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 8px;
+  padding: 6px 12px;
+  background-color: #ff4040;
+  font-size: ${({ theme }) => theme.fontSize.medium14}px;
+  font-weight: 500;
+`;
+const LabelBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`;
+
+const SaveBtn = styled.div`
+  background-color: #de1a1a;
+  color: white;
+  width: 150px;
+  border-radius: 8px;
+  margin: 0 16px;
+  height: 50px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const BottomContainer = styled.div`
+  margin: 16px 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `;
