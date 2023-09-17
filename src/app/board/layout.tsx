0e4afdc6f2c68b95551b9a65079124e55edf0ad0
YeaJeong <bananana0118@gmail.com>
2023-09-17 13:30:26 +0900

    Revert "CHEC-37 Detail Modal UI 세팅"

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -1,24 +1,12 @@
-"use client"
-
-import NavBar from "@/components/ui/NavBar";
-import styled from "@emotion/styled";
-
-
 export default function BoardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <Container>
-      <NavBar/>{children}
-    </Container>
+    <div>
+      <h1>board title</h1>
+      {children}
+    </div>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  height: 100vh;
-  flex-direction: row;
-  justify-content: start;
-`;
